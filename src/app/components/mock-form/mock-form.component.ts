ddbb5de8bc10816ca33f877b92c633f98adfaeb8
Damian Sire <damiansire@gmail.com>
2024-03-10 03:37:33 -0300

    feat: generate date beetween range

--- src/app/components/mock-form/mock-form.component.ts
+++ src/app/components/mock-form/mock-form.component.ts
@@ -1,12 +1,54 @@
-import { Component } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-mock-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './mock-form.component.html',
-  styleUrl: './mock-form.component.css'
+  styleUrl: './mock-form.component.css',
+  providers: [DatePipe],
 })
 export class MockFormComponent {
+  constructor(private datePipe: DatePipe) {}
+  @Output() data: EventEmitter<any> = new EventEmitter();
 
+  mockForm = new FormGroup({
+    fromDate: new FormControl('2023-03-08'),
+    toDate: new FormControl('2023-03-15'),
+  });
+
+  generateData(fromDate: string, toDate: string) {
+    const startDate = new Date(fromDate);
+    const endDate = new Date(toDate);
+    const data = [];
+
+    let currentDate = startDate;
+    while (currentDate <= endDate) {
+      const dataItem = {
+        id: Math.floor(Math.random() * 1000),
+        date: this.dateToUtc(currentDate),
+      };
+      data.push(dataItem);
+
+      currentDate.setDate(currentDate.getDate() + 1);
+    }
+    return data;
+  }
+
+  dateToUtc(date: Date) {
+    return `${date.getUTCDate()} ${
+      date.getUTCMonth() + 1
+    } ${date.getUTCFullYear()}`;
+  }
+
+  onSubmit() {
+    const fromDateValue = this.mockForm.value.fromDate;
+    const toDateValue = this.mockForm.value.toDate;
+    if (fromDateValue && toDateValue) {
+      this.generateData(fromDateValue, toDateValue);
+    }
+    console.warn(this.mockForm.value);
+  }
 }
