2fca9d8ec13c48bb53941ff2c88c5c3bb687b21e
Damian Sire <damiansire@gmail.com>
2024-03-10 03:37:14 -0300

    feat: add mock form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MockParametersComponent } from './components/mock-parameters/mock-parameters.component';
+import { MockPreviewComponent } from './components/mock-preview/mock-preview.component';
+import { MockFormComponent } from './components/mock-form/mock-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [
+    RouterOutlet,
+    MockParametersComponent,
+    MockPreviewComponent,
+    MockFormComponent,
+  ],
 })
 export class AppComponent {
   title = 'mock-creator';
